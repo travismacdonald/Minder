65510a7e46316721619395d764d649b9c6c156c5
travismacdonald <macdonaldtravisc@gmail.com>
2022-05-14 16:50:50 -0300

    Add 'Your Reminders' title.
    - Add compose functions for reminder title
    - Add compose preview function for layout

--- app/src/main/java/com/cannonballapps/minder/MainActivity.kt
+++ app/src/main/java/com/cannonballapps/minder/MainActivity.kt
@@ -3,41 +3,67 @@ package com.cannonballapps.minder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cannonballapps.minder.ui.theme.MinderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MinderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            RemindersListLayout()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RemindersListLayout() {
+    MinderTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            RemindersListTitleLayout()
+        }
+    }
+}
+
+@Composable
+fun RemindersListTitleLayout() {
+    Row(
+        horizontalArrangement = Arrangement.Center
+
+    ) {
+        RemindersListTitle()
+    }
+}
+
+@Composable
+fun RemindersListTitle() {
+    Text(
+        text = stringResource(R.string.reminders_list_title),
+        fontSize = 24.sp,
+        modifier = Modifier
+            .padding(vertical = 12.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PreviewRemindersListTitle() {
     MinderTheme {
-        Greeting("Android")
+        RemindersListLayout()
     }
 }
