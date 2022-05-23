333a9467c5f9d26810c6c9a86a5c954dd7ac7ece
travismacdonald <macdonaldtravisc@gmail.com>
2022-05-22 21:47:28 -0300

    wip: reminder list

--- app/src/main/java/com/cannonballapps/minder/MainActivity.kt
+++ app/src/main/java/com/cannonballapps/minder/MainActivity.kt
@@ -3,15 +3,17 @@ package com.cannonballapps.minder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,12 +32,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RemindersListLayout() {
     MinderTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
             RemindersListTitleLayout()
+            RemindersList(sampleList())
+        }
+    }
+}
+
+@Composable
+fun RemindersList(reminderItems: List<ReminderItem>) {
+    LazyColumn {
+        items(reminderItems) { reminder ->
+            RemindersListItem(item = reminder)
         }
     }
 }
@@ -44,7 +55,6 @@ fun RemindersListLayout() {
 fun RemindersListTitleLayout() {
     Row(
         horizontalArrangement = Arrangement.Center
-
     ) {
         RemindersListTitle()
     }
@@ -60,10 +70,64 @@ fun RemindersListTitle() {
     )
 }
 
+// TODO border color
+// TODO extract hardcoded dimens
+@Composable
+fun RemindersListItem(item: ReminderItem) {
+    Surface(
+        Modifier
+            .fillMaxWidth(),
+        border = BorderStroke(width = 2.dp, color = Color.Cyan),
+        shape = RoundedCornerShape(15),
+    ) {
+        Column {
+            Row {
+                Text(
+                    item.name,
+                    modifier = Modifier.padding(all = 8.dp),
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewRemindersListTitle() {
     MinderTheme {
         RemindersListLayout()
     }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Reminders List Item",
+    widthDp = 400,
+)
+@Composable
+fun PreviewRemindersListItem() {
+    MinderTheme {
+        RemindersListItem(
+            ReminderItem("Drink Water"),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewRemindersList() {
+    MinderTheme {
+        RemindersListLayout()
+    }
+}
+
+
+data class ReminderItem(val name: String)
+
+val sampleList = {
+    val items = mutableListOf<ReminderItem>()
+    for (i in 0..100) {
+        items.add(ReminderItem("Item $i"))
+    }
+    items
 }
