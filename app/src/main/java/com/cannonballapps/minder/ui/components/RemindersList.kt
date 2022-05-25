468a98a3d12da36b3c9e28b0f8eb3900f870ee35
travismacdonald <macdonaldtravisc@gmail.com>
2022-05-25 06:31:21 -0300

    refactor: separate compose functions to individual files

--- app/src/main/java/com/cannonballapps/minder/ui/components/RemindersList.kt
+++ app/src/main/java/com/cannonballapps/minder/ui/components/RemindersList.kt
@@ -1,19 +1,10 @@
 package com.cannonballapps.minder.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.cannonballapps.minder.R
 import com.cannonballapps.minder.ui.theme.MinderTheme
 
 @Composable
@@ -26,33 +17,5 @@ fun RemindersListLayout() {
         ) {
             RemindersListTitleLayout()
         }
-    }
-}
-
-@Composable
-fun RemindersListTitleLayout() {
-    Row(
-        horizontalArrangement = Arrangement.Center
-
-    ) {
-        RemindersListTitle()
-    }
-}
-
-@Composable
-fun RemindersListTitle() {
-    Text(
-        text = stringResource(R.string.reminders_list_title),
-        fontSize = 24.sp,
-        modifier = Modifier
-            .padding(vertical = 12.dp)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewRemindersListTitle() {
-    MinderTheme {
-        RemindersListLayout()
     }
 }
